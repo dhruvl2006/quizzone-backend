278b1863a2ed2f1ab23d0e82ff7f631df386c36b
dhruvl2006 <lohar.dhruv07@gmail.com>
2024-08-04 11:15:27 +0530

    add cors options

--- auth.js
+++ auth.js
@@ -30,6 +30,13 @@ app.options(
 );
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
